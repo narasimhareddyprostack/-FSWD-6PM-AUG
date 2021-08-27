df00d6c3a0a38ab2c7610f6f1f45c169c8e95142
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-08-27 19:11:09 +0530

    web

---
+++ JS-Complete/JS-Basics/Employee.js
@@ -0,0 +1,13 @@
+// single line comments
+/*
+  Multiliine comments 
+   Shift + Alt + A
+*/
+
+var x = 10;
+var y = 20;
+console.log(x + y);
+/*
+    - JS keyword/Reserved Words 
+    - some of the words are reserved for specific purpose.
+*/
